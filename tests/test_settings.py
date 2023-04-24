fe446e987eb22aba0d7e7db8965dab3f4308dde3
Дмитрий Меркулов <night-watch07@mail.ru>
2023-04-24 16:39:34 +0300

    24.04.2023

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,11 +1,11 @@
 from api_yamdb import settings
 
-#
-#class TestSettings:
 
-    #def test_settings(self):
+class TestSettings:
 
-        #assert not settings.DEBUG, 'Проверьте, что DEBUG в настройках Django выключен'
-        #assert settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql', (
-            #'Проверьте, что используете базу данных postgresql'
-       # )
+    def test_settings(self):
+
+        assert not settings.DEBUG, 'Проверьте, что DEBUG в настройках Django выключен'
+        assert settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql', (
+            'Проверьте, что используете базу данных postgresql'
+        )
